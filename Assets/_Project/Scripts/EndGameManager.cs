fa66093026ee1865bd428e7552f86937b6ea00bf
X2SHADOW <hahandrew88@gmail.com>
2025-10-05 21:16:03 +0700

    Update EndGameManager.cs

--- Assets/_Project/Scripts/EndGameManager.cs
+++ Assets/_Project/Scripts/EndGameManager.cs
@@ -1,24 +1,45 @@
+using System.Collections;
 using UnityEngine;
 
 public class EndGameManager : MonoBehaviour
 {
     public static EndGameManager Instance;
-    //public GameObject endGameCanvas; // UI для финала
+    public GameObject endGameWords; // UI для финала
+    public GameObject credits; // UI для финала
+    private ScreenFader screenFader;
 
     private void Awake()
     {
         if (Instance != null) Destroy(this.gameObject);
         else Instance = this;
+        if (screenFader == null) screenFader = FindObjectOfType<ScreenFader>();
     }
 
     public void EndGame()
     {
         Debug.Log("END GAME triggered");
-        //if (endGameCanvas != null) endGameCanvas.SetActive(true);
+        StartCoroutine(EndGameCoroutine());
         // блокируем ввод игрока
         //var player = FindObjectOfType<PlayerController>();
         //if (player != null) player.SetInputBlocked(true);
 
         // здесь можно: показать credits, сохранить прогресс, загрузить сцену и т.д.
+    }
+
+    private IEnumerator EndGameCoroutine()
+    {
+        yield return new WaitForSecondsRealtime(3f);
+
+        yield return screenFader.FadeIn(0.25f);
+
+        endGameWords.SetActive(true);
+
+        yield return new WaitForSecondsRealtime(1f);
+
+        endGameWords.SetActive(false);
+
+        yield return new WaitForSecondsRealtime(1f);
+
+        credits.SetActive(true);
     }
 }
